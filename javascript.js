838c35604995b56eebbdf426deef7a35eb1d7c31
ahmedelmansi-tech <ahmed.helmii911@gmail.com>
2025-07-24 03:09:39 +0300

    first Commit

---
+++ javascript.js
@@ -0,0 +1,35 @@
+const print = console.log;
+
+
+let bar = document.querySelector(".container");
+let percentageNum = document.querySelector(".num");
+let arrow = document.querySelector(".container .fa-arrow-down-long")
+bar.addEventListener("click", ()=>{
+
+    let processPercentage = 0;
+    let processInterval = setInterval(()=>{
+        processPercentage += 1
+        percentageNum.innerHTML = processPercentage + "%";
+
+        
+     bar.style.background = `conic-gradient(peru ${processPercentage}%, #efefef ${processPercentage}% 100%)`;
+   
+       
+        if (processPercentage == 100){
+            clearInterval(processInterval);
+            percentageNum.innerHTML = `Done`;
+            bar.style.background = `green`;
+
+        }
+    },100)
+
+
+    // move the arrow
+    arrow.style.transform = `translate(-50%, 220%)`;
+    percentageNum.style.display = `block`
+    
+})
+
+
+
+print(arrow)
